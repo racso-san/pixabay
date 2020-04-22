f17f1935b64ccbbc8df700e6ee2db459910cbc27
oscar <oll301189@gmail.com>
2020-04-22 14:53:34 -0300

    uno

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Formulario from './components/Formulario';
+
+function App() {
+
+
+  return (
+  
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center">Buscador de Imagenes
+        </p>
+
+        <Formulario/>
+
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
