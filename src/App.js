60a5cb28394288b85a7fa02e449574a192d2f892
oscar <oll301189@gmail.com>
2020-04-23 19:19:31 -0300

    dos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import Formulario from './components/Formulario';
+import ListadoImagenes from './components/ListadoImagenes';
+import { match } from 'minimatch';
 
 function App() {
 
+  const [busqueda,guardarBusqueda] = useState('');
+  const [imagenes, guardarImagenes] = useState([]);
+  const [paginaactual, guardarPaginaActual] = useState(1); // Paginador
+  const [totalpaginas, guardarTotalPaginas] = useState(1); // Paginador
+
+
+  useEffect(() => {
+    const consultarAPI = async() =>{
+      if(busqueda==='') return; // No trae valores en la primera
+
+      const imagenesPorPagina = 30;
+      const key = '1732750-d45b5378879d1e877cd1d35a6';
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+      //  const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}&page=${paginaactual}`;
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      guardarImagenes(resultado.hits);
+
+      // Caclcular el total de paginas
+      const calcularTotalPaginas = Math.ceil(resultado.totalHits / imagenesPorPagina);
+      guardarTotalPaginas(calcularTotalPaginas);
+    }
+    consultarAPI();
+  }, [busqueda]);
 
   return (
   
@@ -11,9 +38,18 @@ function App() {
         <p className="lead text-center">Buscador de Imagenes
         </p>
 
-        <Formulario/>
+        <Formulario
+          guardarBusqueda={guardarBusqueda}
+        />
 
       </div>
+
+      <div className="row justify-content-center">
+        <ListadoImagenes
+          imagenes={imagenes}
+        />
+      </div>
+
     </div>
 
   );
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Error from './Error';
 
-const Formulario = () => {
+const Formulario = ({guardarBusqueda}) => {
+
+
+    const [termino,guardarTermino] = useState('');
+    const [error,guardarError] = useState(false);
+
+    const buscarImagenes = (e) => {
+        e.preventDefault();
+        
+        if(termino.trim()===''){
+            guardarError(true);
+            return;
+        }
+
+        guardarError(false);
+        guardarBusqueda(termino);
+
+    };
 
 
     return ( 
 
-        <form>
+        <form
+            onSubmit={buscarImagenes}
+        >
             <div className="row">
                 <div className="form-group col-md-8">
                     <input
                         type="text"
                         className="form-control form-control-lg"
                         placeholder="Buscar una imagen, ejemplo: futbol o cafe"
+                        onChange={e => guardarTermino(e.target.value)}
                     />
                 </div>
 
@@ -24,6 +45,9 @@ const Formulario = () => {
                 </div>    
 
             </div>
+
+            {error ? <Error mensaje="Agrega un termino de busqueda"/> : null}
+
         </form>
 
      );
